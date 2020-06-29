d991e886bffdcebcd968fc2ceec6774855f078b9
yjin910 <yoojin9160@gmail.com>
2020-06-29 07:03:34 +0100

    Final install_opencv.sh

--- install_opencv.sh
+++ install_opencv.sh
@@ -10,9 +10,9 @@ wget https://github.com/pjreddie/darknet/blob/master/data/coco.names?raw=true -O
 
 echo "Install OpenCV"
 
-# Clean build directories
+# Clean opencv
 cd ~/
-rm -rf ~/opencv/build
+rm -rf ~/opencv
 
 # Update packages
 
@@ -29,31 +29,18 @@ echo "---------------------"
 echo "Install OS Libraries"
 
 sudo apt-get -y install build-essential cmake
-sudo apt-get -y install cmake
-sudo apt-get -y install pkg-config
-
-sudo apt-get -y install libpng12-dev libjpeg-dev libtiff5-dev libjasper-dev
-sudo apt-get -y install libavformat-dev libxvidcore-dev libswscale-dev libxine2-dev libavcodec-dev libx264-dev
-sudo apt-get -y install libv4l-dev v4l-utils libpng-dev libtiff-dev
+sudo apt-get -y install libjpeg-dev libtiff5-dev libjasper-dev libpng12-dev
+sudo apt-get -y install libavcodec-dev libavformat-dev libswscale-dev libxvidcore-dev libx264-dev libxine2-dev
+sudo apt-get -y install libv4l-dev v4l-utils
 sudo apt-get -y install libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev
-sudo apt-get -y install libqt4-dev mesa-utils libgl1-mesa-dri libqt4-opengl-dev libgtk-3-dev
+sudo apt-get -y install libgtk2.0-dev
+sudo apt-get -y install mesa-utils libgl1-mesa-dri libgtkg12.0-dev libgtkglext1-dev
 sudo apt-get -y install libatlas-base-dev gfortran libeigen3-dev
-sudo apt-get -y install libopencv-core-dev libfontconfig1-dev libcairo2-dev
-sudo apt-get -y install libgdk-pixbuf2.0-dev libpango1.0-dev libgtk2.0-dev 
-sudo apt-get -y install libqtgui4 libqtwebkit4 libqt4-test python3-pyqt5 ffmpeg
-
-# Install Python Libraries
-
-sudo apt-get -y install python-dev python2.7-dev python3-dev libpython3-dev libpython3.4-dev libpython3.5-dev python-numpy python3-numpy
-
+sudo apt-get -y install python2.7-dev python3-dev python-numpy python3-numpy
 
 # Download OpenCV and OpenCV_Contrib
 mkdir opencv
 cd opencv
-rm opencv.zip
-rm opencv_contrib.zip
-rm -rf opencv-4.2.0
-rm -rf opencv_contrib-4.2.0
 
 wget -O opencv.zip https://github.com/opencv/opencv/archive/4.2.0.zip
 unzip opencv.zip
@@ -75,6 +62,7 @@ cmake -D CMAKE_BUILD_TYPE=RELEASE \
       -D CMAKE_INSTALL_PREFIX=/usr/local \
       -D WITH_TBB=OFF \
       -D WITH_IPP=OFF \
+      -D WITH_1394=OFF \
       -D BUILD_WITH_DEBUG_INFO=OFF \
       -D BUILD_DOCS=OFF \
       -D INSTALL_C_EXAMPLES=ON \
@@ -83,25 +71,28 @@ cmake -D CMAKE_BUILD_TYPE=RELEASE \
       -D BUILD_TESTS=OFF \
       -D BUILD_PERF_TESTS=OFF \
       -D ENABLE_NEON=ON \
-      -D WITH_QT=ON \
+      -D WITH_QT=OFF \
+      -D WITH_GTK=ON \
       -D WITH_OPENGL=ON \
+      -D OPENCV_ENABLE_NONFREE=ON \
       -D OPENCV_EXTRA_MODULES_PATH=../../opencv_contrib-4.2.0/modules \
       -D WITH_V4L=ON \
       -D WITH_FFMPEG=ON \
       -D WITH_XINE=ON \
+      -D ENABLE_PRECOMPILED_HEADERS=OFF \
       -D BUILD_NEW_PYTHON_SUPPORT=ON \
-      -D PYTHON2_INCLUDE_DIR=/usr/include/python2.7 \
-      -D PYTHON2_NUMPY_INCLUDE_DIRS=/usr/lib/python2.7/dist-packages/numpy/core/include/ \
-      -D PYTHON2_PACKAGES_PATH=/usr/lib/python2.7/dist-packages \
-      -D PYTHON2_LIBRARY=/usr/lib/arm-linux-gnueabihf/libpython2.7.so \
-      -D PYTHON3_INCLUDE_DIR=/usr/include/python3.5m \
-      -D PYTHON3_NUMPY_INCLUDE_DIRS=/usr/lib/python3/dist-packages/numpy/core/include/ \
-      -D PYTHON3_PACKAGES_PATH=/usr/lib/python3.5/dist-packages \
-      -D PYTHON3_LIBRARY=/usr/lib/arm-linux-gnueabihf/libpython3.5m.so \
-      ../
+      -D OPENCV_GENERATE_PKGCONFIG=ON ../
 
+# Reset swap file
 
-make -j$(nproc) 
+echo "Reset swap file"
+sudo sed -i 's/CONF_SWAPSIZE=100/CONF_SWAPSIZE=1024/g' /etc/dphys-swapfile
+sudo /etc/init.d/dphys-swapfile stop
+sudo /etc/init.d/dphys-swapfile start
+
+# make
+
+make
 sudo make install 
 
 sudo sh -c 'echo '/usr/local/lib' > /etc/ld.so.conf.d/opencv.conf'
@@ -119,8 +110,5 @@ sudo /etc/init.d/dphys-swapfile start
 
 echo "sudo modprobe bcm2835-v4l2" >> ~/.profile
 
-
-# create opencv4 file
-sudo cp -rf opencv4.pc /usr/local/lib/
 
 echo "Done!"
